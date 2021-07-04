c34f625d9937ae83157179089815ba82b3ee7ae2
mvarga02 <vargamarcell@gmail.com>
2021-07-04 19:58:03 +0200

    Initial commit

---
+++ src/main/java/org/voucher/voucherservice/VoucherServiceApplication.java
@@ -0,0 +1,13 @@
+package org.voucher.voucherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherServiceApplication.class, args);
+	}
+
+}
